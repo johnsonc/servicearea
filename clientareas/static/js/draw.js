8c866f554dcae2e02d9393c07520ff4f794ecef8
Rafael Capdevielle <capdevielle@gmail.com>
2014-12-26 16:30:31 -0300

    Added draw map initial functionality

--- clientareas/static/js/draw.js
+++ clientareas/static/js/draw.js
@@ -1,26 +1,96 @@
 (function($){
-    function initialize() {
-      var mapOptions = {
-        center: new google.maps.LatLng(-34.397, 150.644),
-        zoom: 8
-      };
+      var drawingManager;
+      var selectedShape;
 
-      var map = new google.maps.Map(document.getElementById('map_canvas'),
-        mapOptions);
-
-      var drawingManager = new google.maps.drawing.DrawingManager({
-        drawingMode: google.maps.drawing.OverlayType.MARKER,
-        drawingControl: true,
-        drawingControlOptions: {
-          position: google.maps.ControlPosition.TOP_CENTER,
-          drawingModes: [
-            google.maps.drawing.OverlayType.POLYGON,
-          ]
+      function clearSelection() {
+        if (selectedShape) {
+          selectedShape.setEditable(false);
+          selectedShape = null;
         }
-      });
-      drawingManager.setMap(map);
-    }
+      }
 
-    google.maps.event.addDomListener(window, 'load', initialize);
+      function setSelection(shape) {
+        clearSelection();
+        selectedShape = shape;
+        shape.setEditable(true);
+        window.shape = selectedShape;
+        if (selectedShape) {
+          console.log(selectedShape.getPath().j)
+        }
+      }
+
+      function deleteSelectedShape() {
+        if (selectedShape) {
+          selectedShape.setMap(null);
+        }
+        // To show:
+         drawingManager.setOptions({
+           drawingControl: true
+         });
+      }
+
+      function initialize() {
+        var map = new google.maps.Map(document.getElementById('map_canvas'), {
+          zoom: 10,
+          center: new google.maps.LatLng(22.344, 114.048),
+          mapTypeId: google.maps.MapTypeId.ROADMAP,
+          disableDefaultUI: true,
+          zoomControl: true
+        });
+
+        var polyOptions = {
+          strokeWeight: 0,
+          fillOpacity: 0.45,
+          editable: true
+        };
+        // Creates a drawing manager attached to the map that allows the user to draw
+        // markers, lines, and shapes.
+        drawingManager = new google.maps.drawing.DrawingManager({
+          drawingMode: google.maps.drawing.OverlayType.POLYGON,
+          drawingControlOptions: {
+            drawingModes: [
+              google.maps.drawing.OverlayType.POLYGON
+            ]
+          },
+          markerOptions: {
+            draggable: true
+          },
+          polylineOptions: {
+            editable: true
+          },
+          rectangleOptions: polyOptions,
+          circleOptions: polyOptions,
+          polygonOptions: polyOptions,
+          map: map
+        });
+
+        google.maps.event.addListener(drawingManager, 'overlaycomplete', function(e) {
+            if (e.type != google.maps.drawing.OverlayType.MARKER) {
+            // Switch back to non-drawing mode after drawing a shape.
+            drawingManager.setDrawingMode(null);
+            // To hide:
+            drawingManager.setOptions({
+              drawingControl: false
+            });
+
+            // Add an event listener that selects the newly-drawn shape when the user
+            // mouses down on it.
+            var newShape = e.overlay;
+            newShape.type = e.type;
+            google.maps.event.addListener(newShape, 'click', function() {
+              setSelection(newShape);
+            });
+            setSelection(newShape);
+          }
+        });
+
+        // Clear the current selection when the drawing mode is changed, or when the
+        // map is clicked.
+        google.maps.event.addListener(drawingManager, 'drawingmode_changed', clearSelection);
+        google.maps.event.addListener(map, 'click', clearSelection);
+
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+
 
 })($);
